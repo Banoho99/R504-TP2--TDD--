53b1807e30cf90d2f901765be11870430cb9ab8b
Banoho99 <87777532+Banoho99@users.noreply.github.com>
2025-11-08 14:37:52 +0100

    "Feat: support de affiche(n1, n2) + refactor avec _fb_token"
    "Feat: support de affiche(n1, n2) + refactor avec _fb_token"

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,29 @@
-def affiche(n=100):
-    result = []
-    for i in range(1, n + 1):
-        if i % 15 == 0:
-            result.append("FrisBee")
-        elif i % 3 == 0:
-            result.append("Fizz")
-        elif i % 5 == 0:
-            result.append("Buzz")
-        else:
-            result.append(str(i))
-    return "".join(result)
+def _fb_token(i: int) -> str:
+    if i % 15 == 0:
+        return "FrisBee"
+    if i % 3 == 0:
+        return "Fizz"
+    if i % 5 == 0:
+        return "Buzz"
+    return str(i)
+
+def affiche(*args):
+    """
+    - affiche()        -> concatène 1..100
+    - affiche(n)       -> concatène 1..n
+    - affiche(n1, n2)  -> concatène n1..n2 (inclus)
+    """
+    if len(args) == 0:
+        start, end = 1, 100
+    elif len(args) == 1:
+        n = args[0]
+        start, end = 1, int(n)
+    elif len(args) == 2:
+        n1, n2 = args
+        start, end = int(n1), int(n2)
+        if start > end:
+            raise ValueError("affiche(n1, n2): n1 doit être <= n2")
+    else:
+        raise TypeError("affiche() accepte 0, 1 ou 2 arguments")
+
+    return "".join(_fb_token(i) for i in range(start, end + 1))
